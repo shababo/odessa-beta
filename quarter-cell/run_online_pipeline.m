733a25c048233b4abbdf697d80e0246a5a8a0aff
ChenShizhe <szchen@uw.edu>
2017-11-10 13:49:29 -0500

    Editted the run_online_pipeline

--- quarter-cell/run_online_pipeline.m
+++ quarter-cell/run_online_pipeline.m
@@ -1,5 +1,6 @@
 function run_online_pipeline(exp_query_filename)
 
+
 load(exp_query_filename)
 % neighbourhood, experiment_query, experiment_setup
 
@@ -30,20 +31,23 @@ for i = 1:length(group_names)
     
     this_group = group_names{i};
     this_exp_query = experiment_query.(this_group);
-    
-    neighbourhood = ...
-        experiment_setup.groups.(this_group).inference_function(this_exp_query,neighbourhood,experiment_setup);
+    group_profile=experiment_setup.groups.(this_group);
+
+    this_neighbourhood = ...
+        experiment_setup.groups.(this_group).inference_function(this_exp_query,this_neighbourhood,group_profile, experiment_setup.prior_info);
     
 end
 
 % regroup cells
 for i = 1:length(group_names)
-    for j = setdiff(1:length(group_names),i)
-        this_group = group_names{i};
-        to_group = group_names{j};
-        regroup_func = experiment_setup.groups.(this_group).regroup_functions(to_group);
-        neighbourhood = regroup_func(neighbourhood);
-    end
+    %for j = setdiff(1:length(group_names),i)
+    this_group = group_names{i};
+    %         to_group = group_names{j};
+    to_groups=setdiff(group_names,this_group);
+    group_profile=experiment_setup.groups.(this_group);
+    %   regroup_func = experiment_setup.groups.(this_group).regroup_functions(to_group);
+    neighbourhood  = experiment_setup.groups.(this_group).regroup_functions(neighbourhood,to_groups,group_profile);
+%     end
 end
 
 % AT THIS POINT WRITE OUT TO EXPERIMENT RECORD
