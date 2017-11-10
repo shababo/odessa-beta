310e6ac22469f696f1c591d07ea1fc385dc5a18d
shababo <benshababo@gmail.com>
2017-11-09 16:42:35 -0800

    more tweaks

--- run_mapping_experiment.m
+++ run_mapping_experiment.m
@@ -138,8 +138,10 @@ if is_exp && ~experiment_setup.exp.sim_locs
     [handles, experiment_setup] = detect_nucs_analysis_comp(hObject,handles,acq_gui,acq_gui_data,experiment_setup);
     [acq_gui, acq_gui_data] = get_acq_gui_data;
 else
-    experiment_setup = sim_cell_positions(experiment_setup);
+    experiment_setup = sim_cells(experiment_setup);
 end
+
+
 
 if experiment_setup.is_exp
     handles = create_neighbourhoods_caller(hObject,handles,acq_gui,acq_gui_data,experiment_setup);
@@ -162,16 +164,20 @@ if experiment_setup.is_exp
 
     handles = setup_patches(hObject,eventdata,handles,acq_gui,acq_gui_data,experiment_setup);
     [acq_gui, acq_gui_data] = get_acq_gui_data;
+    
+    % compute bg rate from data
 
     set(acq_gui_data.test_pulse,'Value',1)
     set(acq_gui_data.loop,'Value',1)
     set(acq_gui_data.tf_on,'Value',get(handles.tf_flag,'Value'));
     set(acq_gui_data.trigger_seq,'Value',1)
     set(acq_gui_data.loop_count,'String',num2str(1))
+else
+    % simulate bg rate
 end
 num_map_locations = length(neighbourhoods);
 
-
+not_terminated = 1;
 while not_terminated
     for i = 1:num_map_locations
     
@@ -190,82 +196,87 @@ while not_terminated
         % Run the designed trials
         
         
-        
-        do_run_trials = 1;
-        if experiment_setup.enable_user_breaks
-            choice = questdlg('Run the trials?', ...
-                'Run the trials?', ...
-                'Yes','No','Yes');
-            % Handle response
-            switch choice
-                case 'Yes'
-                    do_run_trials = 1;
-                    choice = questdlg('Continue user control?',...
-                        'Continue user control?', ...
-                        'Yes','No','Yes');
-                    % Handle response
-                    switch choice
-                        case 'Yes'
-                            experiment_setup.enable_user_breaks = 1;
-                        case 'No'
-                            experiment_setup.enable_user_breaks = 0;
-                    end
-                case 'No'
-                    do_run_trials = 0;
-            end
-        end
-        
-        if do_run_trials
-            set(handles.tf_flag,'Value',1)
-            set(handles.set_seq_trigger,'Value',0)
-            set(handles.target_intensity,'String',user_input_powers)
-
-            [handles, acq_gui, acq_gui_data] = build_seq_groups(hObject, eventdata, handles);
-        %     handles = guidata(hObject);
-        %     acq_gui_data = get_acq_gui_data();
-            max_seq_length = str2double(get(handles.max_seq_length,'String'));
-            this_seq = acq_gui_data.data.sequence;
-            num_runs = ceil(length(this_seq)/max_seq_length);
-            handles.data.start_trial = acq_gui_data.data.sweep_counter + 1;
-
-            for run_i = 1:num_runs
-
-                this_subseq = this_seq((run_i-1)*max_seq_length+1:min(run_i*max_seq_length,length(this_seq)));
-                time_offset = this_subseq(1).start - 1000;
-                for k = 1:length(this_subseq)
-                    this_subseq(k).start = this_subseq(k).start - time_offset;
+        if is_exp
+            do_run_trials = 1;
+            if experiment_setup.enable_user_breaks
+                choice = questdlg('Run the trials?', ...
+                    'Run the trials?', ...
+                    'Yes','No','Yes');
+                % Handle response
+                switch choice
+                    case 'Yes'
+                        do_run_trials = 1;
+                        choice = questdlg('Continue user control?',...
+                            'Continue user control?', ...
+                            'Yes','No','Yes');
+                        % Handle response
+                        switch choice
+                            case 'Yes'
+                                experiment_setup.enable_user_breaks = 1;
+                            case 'No'
+                                experiment_setup.enable_user_breaks = 0;
+                        end
+                    case 'No'
+                        do_run_trials = 0;
                 end
-                total_duration = (this_subseq(end).start + this_subseq(end).duration)/1000 + 5;
-
-                set(acq_gui_data.trial_length,'String',num2str(total_duration + 1.0))
-                acq_gui_data = Acq('trial_length_Callback',acq_gui_data.trial_length,eventdata,acq_gui_data);
-                instruction = struct();
-                instruction.type = 32; %SEND SEQ
-                handles.sequence = this_subseq;
-                instruction.sequence = this_subseq;
-                handles.total_duration = total_duration;
-                instruction.waittime = total_duration + 120;
-                disp('sending instruction...')
-                [return_info,success,handles] = do_instruction_slidebook(instruction,handles);
-        %         acq_gui_data = get_acq_gui_data();
-        %         acq_gui_data.data.stim_key =  return_info.stim_key;
-                acq_gui_data.data.sequence =  this_subseq;
-        %         acq_gui = findobj('Tag','acq_gui');
-                guidata(acq_gui,acq_gui_data)
-
-                set(acq_gui_data.trial_length,'String',num2str(handles.total_duration + 1.0))
-                acq_gui_data = Acq('trial_length_Callback',acq_gui_data.trial_length,eventdata,acq_gui_data);
-
-                guidata(hObject,handles)
-                exp_data = handles.data; save(handles.data.experiment_setup.fullsavefile,'exp_data')
-        %         guidata(acq_gui,acq_gui_data)
-
-                acq_gui_data = Acq('run_Callback',acq_gui_data.run,eventdata,acq_gui_data);
-                waitfor(acq_gui_data.run,'String','Start')
-                guidata(acq_gui,acq_gui_data)
-
             end
+
+            if do_run_trials
+                set(handles.tf_flag,'Value',1)
+                set(handles.set_seq_trigger,'Value',0)
+                set(handles.target_intensity,'String',user_input_powers)
+
+                [handles, acq_gui, acq_gui_data] = build_seq_groups(hObject, eventdata, handles);
+            %     handles = guidata(hObject);
+            %     acq_gui_data = get_acq_gui_data();
+                max_seq_length = str2double(get(handles.max_seq_length,'String'));
+                this_seq = acq_gui_data.data.sequence;
+                num_runs = ceil(length(this_seq)/max_seq_length);
+                handles.data.start_trial = acq_gui_data.data.sweep_counter + 1;
+
+                for run_i = 1:num_runs
+
+                    this_subseq = this_seq((run_i-1)*max_seq_length+1:min(run_i*max_seq_length,length(this_seq)));
+                    time_offset = this_subseq(1).start - 1000;
+                    for k = 1:length(this_subseq)
+                        this_subseq(k).start = this_subseq(k).start - time_offset;
+                    end
+                    total_duration = (this_subseq(end).start + this_subseq(end).duration)/1000 + 5;
+
+                    set(acq_gui_data.trial_length,'String',num2str(total_duration + 1.0))
+                    acq_gui_data = Acq('trial_length_Callback',acq_gui_data.trial_length,eventdata,acq_gui_data);
+                    instruction = struct();
+                    instruction.type = 32; %SEND SEQ
+                    handles.sequence = this_subseq;
+                    instruction.sequence = this_subseq;
+                    handles.total_duration = total_duration;
+                    instruction.waittime = total_duration + 120;
+                    disp('sending instruction...')
+                    [return_info,success,handles] = do_instruction_slidebook(instruction,handles);
+            %         acq_gui_data = get_acq_gui_data();
+            %         acq_gui_data.data.stim_key =  return_info.stim_key;
+                    acq_gui_data.data.sequence =  this_subseq;
+            %         acq_gui = findobj('Tag','acq_gui');
+                    guidata(acq_gui,acq_gui_data)
+
+                    set(acq_gui_data.trial_length,'String',num2str(handles.total_duration + 1.0))
+                    acq_gui_data = Acq('trial_length_Callback',acq_gui_data.trial_length,eventdata,acq_gui_data);
+
+                    guidata(hObject,handles)
+                    exp_data = handles.data; save(handles.data.experiment_setup.fullsavefile,'exp_data')
+            %         guidata(acq_gui,acq_gui_data)
+
+                    acq_gui_data = Acq('run_Callback',acq_gui_data.run,eventdata,acq_gui_data);
+                    waitfor(acq_gui_data.run,'String','Start')
+                    guidata(acq_gui,acq_gui_data)
+
+                end
+            end
+        else
+            % simulate this batch data
+            
         end
+        
         
         % RUN ONLINE MAPPING PIPELINE HERE
         if experiment_setup.is_exp
