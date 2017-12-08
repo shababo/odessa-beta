6b34ddf76a5a30598de1d0c632e4464a63f91b92
Ben Shababo <benshababo@gmail.com>
2017-12-08 14:18:04 -0800

    dont fit x offset for pockels power curve

--- quarter-cell/fit_pockels_curves.m
+++ quarter-cell/fit_pockels_curves.m
@@ -1,12 +1,12 @@
 function results = fit_pockels_curves(voltages,measurements)
 
-results = zeros(size(measurements,1),3);
+results = zeros(size(measurements,1),2);
 
 
 for i = 1:size(measurements,1)
     
     these_measurements = measurements(i,:);
-    params_init = [max(these_measurements) pi/200/2 0];
+    params_init = [max(these_measurements) pi/200/2];
     
     results(i,:) = fminsearch(@(x) pockels_fit_err(x,[voltages' these_measurements']), params_init);
     
--- quarter-cell/sin_sq.m
+++ quarter-cell/sin_sq.m
@@ -1,3 +1,3 @@
 function y = sin_sq(x, params)
 
-y = params(1) * sin(params(2) * x + params(3)).^2;
+y = params(1) * sin(params(2) * x + 0).^2;
